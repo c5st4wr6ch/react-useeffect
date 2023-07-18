056dcfc456ffacc1de1baf5ee1d3fd3142f8bfec
novi <c5st4wr6ch@live.com>
2023-07-18 21:01:18 +0700

    Initialize project using Create React App

---
+++ build/precache-manifest.856956d9fc2191e24eaf7ccb4f940d2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32c677f339ed64073cd14edc84530b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "943817228b92f2522835",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dbc439a855bbb41da30",
+    "url": "/static/js/2.063cf10e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.063cf10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943817228b92f2522835",
+    "url": "/static/js/main.a65b80fe.chunk.js"
+  },
+  {
+    "revision": "137ccada010c5f0be029",
+    "url": "/static/js/runtime-main.0a240186.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
